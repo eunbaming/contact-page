be628999a5b5c136b02c5c84a160e9509410bca6
kimyuna <dbskqnsghd@naver.com>
2023-02-15 16:30:50 +0900

    add contact

--- src/component/ContactList.js
+++ src/component/ContactList.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import ContactItem from './ContactItem'
 import SearchBox from './SearchBox'
 
 const ContactList = () => {
+  const contactList = useSelector(state => state.contactList)
   return (
     <div>
       <SearchBox />
       <div className='contact-list-item'>
-        <ContactItem />
+        {contactList.map((item) => <ContactItem item={item} />)}
       </div>
     </div>
   )
